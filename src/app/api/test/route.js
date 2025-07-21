33369798aa01d6dd19ad9fe2d7fd0c28c7df2db9
Vincent S. <vincent.silvestri@hotmail.fr>
2025-07-21 16:33:36 +0200

    deployement vercel

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,12 +1,12 @@
-import connectDB from "@/lib/db";
+// import connectDB from "@/lib/db";
 
-export async function GET() {
-  try {
-    await connectDB();
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: "DB not connected" }), {
-      status: 500,
-    });
-  }
-}
+// export async function GET() {
+//   try {
+//     await connectDB();
+//     return new Response(JSON.stringify({ success: true }), { status: 200 });
+//   } catch (err) {
+//     return new Response(JSON.stringify({ error: "DB not connected" }), {
+//       status: 500,
+//     });
+//   }
+// }
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,28 +1,28 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI not set in .env.local");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("MONGODB_URI not set in .env.local");
+// }
 
-let cached = global.mongoose || { conn: null, promise: null };
+// let cached = global.mongoose || { conn: null, promise: null };
 
-async function connectDB() {
-  if (cached.conn) return cached.conn;
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI, {
-        bufferCommands: false,
-      })
-      .then((mongoose) => {
-        return mongoose;
-      });
-  }
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(MONGODB_URI, {
+//         bufferCommands: false,
+//       })
+//       .then((mongoose) => {
+//         return mongoose;
+//       });
+//   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
 
-export default connectDB;
+// export default connectDB;
