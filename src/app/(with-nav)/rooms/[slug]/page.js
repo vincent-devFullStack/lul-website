2b7d727621dd019446254d461f344e21bbce4c37
Damien <damien.mourot.91@gmail.com>
2025-07-23 16:08:24 +0200

    Résolution d'un bug persistant

--- src/app/(with-nav)/rooms/[slug]/page.js
+++ src/app/(with-nav)/rooms/[slug]/page.js
@@ -1,11 +1,10 @@
 // page.js
 import ArtworkSlider from "./ArtworkSlider";
-
+import "./Artworkslider.css";
 export default function RoomPage({ params }) {
   const { slug } = params;
 
   return (
-    <div className="min-h-screen">
       <div className="container mx-auto px-4 py-8">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-serif font-semibold text-gray-800 mb-2">
@@ -15,6 +14,5 @@ export default function RoomPage({ params }) {
         
         <ArtworkSlider />
       </div>
-    </div>
   );
 }
